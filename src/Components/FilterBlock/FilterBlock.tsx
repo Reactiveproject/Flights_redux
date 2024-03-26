1274d832cb4ab05b36d34aa0d58bcf7ff8848d00
Reactiveproject <pavel.yakushenko@gmail.com>
2024-03-26 22:24:19 +0300

    Reorgonize filter blocks with options

--- src/Components/FilterBlock/FilterBlock.tsx
+++ src/Components/FilterBlock/FilterBlock.tsx
@@ -3,38 +3,32 @@ import FilterItem from "../FilterItem/FilterItem";
 import cl from "./FilterBlock.module.scss";
 import { useAppSelector } from "../../Hooks/hooks";
 
+const connectionArray = [
+  { name: "Без пересадок", value: 0 },
+  { name: "1 пересадка", value: 1 },
+  { name: "2 пересадки", value: 2 },
+  { name: "3 пересадки", value: 3 },
+];
+
+const companiesArray = [
+  { name: "Победа", value: "pobeda" },
+  { name: "Red Wins", value: "redwings" },
+  { name: "S7", value: "s7" },
+];
+
 function FilterBlock() {
-  const ticketsdata = useAppSelector((state) => state.flights.flights);
-
-  const connectionsArray = ticketsdata.map((item) => item.connectionAmount);
-  const companiesArray = ticketsdata.map((item) => item.company);
-
-  const uniqueItemArray = (array) => {
-    const uniqArr = array.reduce((acc, item) => {
-      if (acc.includes(item)) {
-        return acc;
-      }
-      return [...acc, item];
-    }, []);
-
-    const result = uniqArr.sort((a, b) => a - b);
-
-    return result;
-  };
-
-  const uniqueConnectionsArray = uniqueItemArray(connectionsArray);
-  const uniqueCompaniesArray = uniqueItemArray(companiesArray);
-
-  console.log(uniqueConnectionsArray);
-  console.log(uniqueCompaniesArray);
-
   return (
     <div className={cl.filterBlock}>
       <FilterItem
         title={"Количество пересадок"}
-        itemArray={uniqueConnectionsArray}
+        itemArray={connectionArray}
+        type={"checkbox"}
       />
-      <FilterItem title={"Компании"} itemArray={uniqueCompaniesArray} />
+      <FilterItem
+        title={"Компании"}
+        itemArray={companiesArray}
+        type={"radio"}
+      />
     </div>
   );
 }
--- src/Components/FilterItem/FilterItem.tsx
+++ src/Components/FilterItem/FilterItem.tsx
@@ -2,14 +2,7 @@ import cl from "./FilterItem.module.scss";
 import { useAppDispatch } from "../../Hooks/hooks";
 import { filteredByFlights } from "../../Store/FlightsSlice";
 
-// const connectArray = [
-//   { name: "Без пересадок", value: 0 },
-//   { name: "1 пересадка", value: 1 },
-//   { name: "2 пересадки", value: 2 },
-//   { name: "3 пересадки", value: 3 },
-// ];
-
-function FilterItem({ title, itemArray }) {
+function FilterItem({ title, itemArray, type }) {
   const dispatch = useAppDispatch();
 
   return (
@@ -20,13 +13,13 @@ function FilterItem({ title, itemArray }) {
           return (
             <div
               className={cl.filterItem}
-              key={item}
+              key={item.name}
               onClick={() => {
-                dispatch(filteredByFlights(item));
+                dispatch(filteredByFlights(item.value));
               }}
             >
               <div className={cl.checkbox}></div>
-              <div className={cl.filterItemTitle}>{item}</div>
+              <div className={cl.filterItemTitle}>{item.name}</div>
             </div>
           );
         })}
